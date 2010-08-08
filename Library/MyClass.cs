a0a190321379af6110494029037deeb1fa6c1d85
unknown <USER@.(none)>
2010-08-08 20:26:20 +0200

    first commit

---
+++ Library/MyClass.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Utworzone przez SharpDevelop.
+ * Użytkownik: USER
+ * Data: 2010-08-08
+ * Godzina: 12:50
+ * 
+ * Do zmiany tego szablonu użyj Narzędzia | Opcje | Kodowanie | Edycja Nagłówków Standardowych.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+
+	
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		public void run()
+		{
+			Console.WriteLine(this.ToString());
+		}
+	}	
+	
+
+}
---
+++ LibraryTest/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Utworzone przez SharpDevelop.
+ * Użytkownik: USER
+ * Data: 2010-08-08
+ * Godzina: 12:50
+ * 
+ * Do zmiany tego szablonu użyj Narzędzia | Opcje | Kodowanie | Edycja Nagłówków Standardowych.
+ */
+
+
+using System;
+using Library;
+
+namespace LibraryTest
+{
+	class Program
+	{
+	
+		public static void Main(string[] args)
+		{		 
+			MyClass l = new MyClass();
+			l.run();
+		}
+	}
+}
